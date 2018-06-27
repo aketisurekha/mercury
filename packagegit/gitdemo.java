1f7afe3c1736e22ac298c0507df9bf27ff07b7f6
aketisurekha <aketisurekha@gmail.com>
2018-06-27 09:29:18 +0530

    finding even number

---
+++ packagegit/gitdemo.java
@@ -0,0 +1,15 @@
+package packagegit;
+
+public class gitdemo {
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++){
+			if(i%2==0){
+				System.out.println(i);
+			}
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
